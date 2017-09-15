ebcb961e09f3cf896985f28572b00d97dc3ac5ac
Fronbasal <danielmalik@outlook.de>
2017-09-15 20:20:58 +0200

    dir => pwd; port flag => arg;

--- serve.go
+++ serve.go
@@ -5,18 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	path = kingpin.Flag("path", "Provide the directory of the folder which you are intending to serve.").Default(".").ExistingDir()
-	port = kingpin.Flag("port", "The port to serve the directory at").Default("3000").Int()
+	port = kingpin.Arg("port", "The port to serve the directory at").Default("3000").Int()
 )
 
 func main() {
 	kingpin.Parse()
-	fmt.Println("Serving " + *path + " on http://localhost:" + strconv.Itoa(*port))
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(*path))))
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println("Serving " + path + " on http://localhost:" + strconv.Itoa(*port))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(path))))
 }
