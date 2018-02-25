1c56a8eadc733b85de4a738c5cccbf723c8b19e0
Fronbasal <danielmalik@outlook.de>
2018-02-25 20:12:31 +0100

    feat(cli): serving on 0.0.0.0

--- serve.go
+++ serve.go
@@ -21,6 +21,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Printf("Serving %s on http://%s:%s \n", path, "localhost", strconv.Itoa(*port))
+	fmt.Printf("Serving %s on http://%s:%s \n", path, "0.0.0.0", strconv.Itoa(*port))
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(path))))
 }
