c22bd6fec6bcab40143cd2a20eaf9543bc26e5a2
Daniel Malik <mail@fronbasal.de>
2018-05-20 16:36:44 +0200

    feat: added directory cli option, added short forms of cli options

--- serve.go
+++ serve.go
@@ -1,26 +1,21 @@
-// Serve: a simple CLI to serve a folder to a port on the interwebz. Use intended for development purposes only!
 package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	port = kingpin.Flag("port", "The port to serve the directory at").Default("3000").Int()
+	port      = kingpin.Flag("port", "The port of the HTTP server.").Default("3000").Short('p').Int()
+	directory = kingpin.Flag("directory", "The directory to serve").Default(".").Short('d').ExistingDir()
 )
 
 func main() {
 	kingpin.Parse()
-	path, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Printf("Serving %s on http://%s:%s \n", path, "0.0.0.0", strconv.Itoa(*port))
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(path))))
+	fmt.Printf("Serving %s on http://%s:%s \n", *directory, "0.0.0.0", strconv.Itoa(*port))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(*directory))))
 }
