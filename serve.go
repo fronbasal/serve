cdc9b12b64be6c2a9ff5c0fb59c7eabc4e028e27
Daniel Malik <mail@fronbasal.de>
2018-08-12 01:26:16 +0200

    feat: implemented logrus, custom connstate handler

--- serve.go
+++ serve.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 	"strconv"
@@ -10,21 +8,39 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
 	"strings"
+	"net"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
 	port      = kingpin.Flag("port", "The port of the HTTP server.").Default("3000").Short('p').Int()
 	directory = kingpin.Arg("directory", "The directory to serve").Default(".").ExistingDir()
+	verbose   = kingpin.Flag("verbose", "Enable verbose output").Default("true").Bool()
 )
 
 func main() {
+	kingpin.CommandLine.Name = "serve"
+	kingpin.CommandLine.Author("Daniel Malik <mail@fronbasal.de>")
 	kingpin.Parse()
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
 	*directory += "/"
 	absDir, err := filepath.Abs(filepath.Dir(*directory))
 	relDir := strings.Replace(absDir, os.Getenv("HOME"), "~", 1) + "/"
 	if err != nil {
-		log.Fatal("Failed to get directory: " + err.Error())
+		log.Fatal("Failed to get directory: ", err.Error())
 	}
-	fmt.Printf("Serving %s on http://%s:%s \n", relDir, "0.0.0.0", strconv.Itoa(*port))
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(absDir))))
+	log.Infof("Serving %s on http://%s:%s \n", relDir, "0.0.0.0", strconv.Itoa(*port))
+	server := &http.Server{Addr: ":" + strconv.Itoa(*port), Handler: http.FileServer(http.Dir(absDir))}
+	server.ConnState = func(conn net.Conn, state http.ConnState) {
+		if state == http.StateActive {
+			log.Debug("Serving client ", conn.LocalAddr().String())
+		}
+	}
+	if server.ListenAndServe() != nil {
+		log.Fatal("Failed to start server")
+	}
 }
